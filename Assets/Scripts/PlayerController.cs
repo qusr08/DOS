0d14c4ef005689bcdf6275a9e07b68468b1248fc
qusr08 <34079525+qusr08@users.noreply.github.com>
2022-07-17 11:51:05 -0400

    Created all levels, added map tile chaining

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour {
 	private Coroutine move = null;
 	private Vector3 movement = Vector3.zero;
 	private readonly Vector3[ ] faces = new Vector3[6];
+	private MapTile currentTile = null;
 
 	public bool IsMoving {
 		get {
@@ -34,8 +35,8 @@ public class PlayerController : MonoBehaviour {
 			// If the input is on a diagonal or not straight on, then ignore it
 			if (movement.magnitude <= 1 && movement.magnitude > 0) {
 				// Check to make sure the direction the player is trying to move in is valid
-				if (IsValidMapTile(movement, out MapTile fromMapTile, out MapTile toMapTile)) {
-					move = StartCoroutine(IMove(movement, fromMapTile, toMapTile));
+				if (IsValidMapTile(movement)) {
+					move = StartCoroutine(IMove(movement));
 				}
 			}
 		}
@@ -46,7 +47,7 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
-	private IEnumerator IMove (Vector3 direction, MapTile fromMapTile, MapTile toMapTile) {
+	private IEnumerator IMove (Vector3 direction) {
 		// TODO: Might have to edit the point if corners of die are "sanded" off
 		Vector3 point = transform.position + ((direction + Vector3.down) / 2f);
 		Vector3 rotationAxis = new Vector3(direction.z, 0, (direction.z == 0 ? -1 : 1) * direction.x);
@@ -63,19 +64,31 @@ public class PlayerController : MonoBehaviour {
 			yield return new WaitForEndOfFrame( );
 		}
 
+		if (currentTile.Type == MapTile.MapTileType.CRACKED) {
+			currentTile.DestroyTile( );
+		}
+
 		// Round values so there are no decimal places and update all face directions
 		UpdateDie( );
+		MapTile prevMapTile;
 
-		if (fromMapTile.Type == MapTile.MapTileType.CRACKED) {
-			fromMapTile.DestroyTile( );
-		}
+		do {
+			prevMapTile = currentTile;
 
-		// The tile that the die has just moved onto might have a special movement/rotation
-		toMapTile.EffectDie(this);
-		// Wait for the tile to finish moving the die
-		while (toMapTile.IsEffectingDie) {
+			// The tile that the die has just moved onto might have a special movement/rotation
+			// Allow the tile to rotate or move the die
+			currentTile.EffectDie(this);
+			// Wait for the tile to finish moving the die
+			while (currentTile.IsEffectingDie) {
+				yield return new WaitForEndOfFrame( );
+			}
+
+			// Get the new current tile that the die is on
+			// It might be the same one as before if the die did not translate
+			UpdateDie( );
+
 			yield return new WaitForEndOfFrame( );
-		}
+		} while (prevMapTile != currentTile);
 
 		move = null;
 	}
@@ -93,6 +106,9 @@ public class PlayerController : MonoBehaviour {
 		faces[3] = transform.right;
 		faces[4] = transform.forward * -1;
 		faces[5] = transform.up * -1;
+
+		// Get the current map tile that the die is standing on
+		currentTile = GetMapTile(transform.position);
 
 		// Debug.DrawRay(transform.position, faces[0], Color.red, 1);
 		// Debug.DrawRay(transform.position, faces[1], Color.blue, 1);
@@ -125,37 +141,33 @@ public class PlayerController : MonoBehaviour {
 		return face;
 	}
 
-	public bool IsValidMapTile (Vector3 direction, out MapTile fromMaptile, out MapTile toMapTile, bool roll = true) {
-		fromMaptile = null;
-		if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit fromHit)) {
-			fromMaptile = fromHit.transform.GetComponent<MapTile>( );
+	public bool IsValidMapTile (Vector3 direction, bool roll = true) {
+		// Check to see if there is a world tile at the position dictated by the direction
+		MapTile mapTile = GetMapTile(transform.position + direction);
+
+		// If there is no map tile the die cannot move in the direction specified
+		if (mapTile == null) {
+			return false;
 		}
 
-		// Check to see if there is a world tile at the position dictated by the direction
-		toMapTile = null;
-		if (Physics.Raycast(transform.position + direction, Vector3.down, out RaycastHit toHit)) {
-			toMapTile = toHit.transform.GetComponent<MapTile>( );
+		// If the tile is being destroyed do not count it as an actual tile
+		if (mapTile.IsDestroyed) {
+			return false;
+		}
 
-			// If there is no map tile the die cannot move in the direction specified
-			if (toMapTile == null) {
-				return false;
-			}
-
-			// If the tile is being destroyed do not count it as an actual tile
-			if (toMapTile.IsDestroyed) {
-				return false;
-			}
-
-			if (toMapTile.ExclusiveFace == 0 || GetNextBottomFace(direction, roll) == toMapTile.ExclusiveFace) {
-				return true;
-			}
+		if (mapTile.ExclusiveFace == 0 || GetNextBottomFace(direction, roll) == mapTile.ExclusiveFace) {
+			return true;
 		}
 
 		return false;
 	}
 
-	public bool IsValidMapTile (Vector3 direction, bool roll = true) {
-		return IsValidMapTile(direction, out MapTile _, out MapTile _, roll);
+	private MapTile GetMapTile (Vector3 position) {
+		if (Physics.Raycast(position, Vector3.down, out RaycastHit hit)) {
+			return hit.transform.GetComponent<MapTile>( );
+		}
+
+		return null;
 	}
 
 	private void OnMove (InputValue inputValue) {
